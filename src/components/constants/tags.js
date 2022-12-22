8e9373c0e1a3ae012b415e2ba530c7b58235cd77
tomlightning <le.tomer@gmail.com>
2022-12-22 15:24:52 +0200

    fix bug and added tag

--- src/components/constants/tags.js
+++ src/components/constants/tags.js
@@ -1,1 +1,11 @@
 export const TAG_SEARCH_TERMS = { dao: ['daos'], nft: ['nfts'], blog: ['blogs'] }
+
+export const TAGS = [
+  { label: 'NFT', url: 'nft' },
+  { label: 'DAOs', url: 'dao' },
+  { label: 'Blogs', url: 'blog' },
+  { label: 'DeFi', url: 'defi' },
+  { label: 'Nimi', url: 'nimi' },
+  { label: 'Planetable', url: 'planetable' },
+  { label: 'ENS', url: 'ens' },
+]
--- src/components/tags/RegularTags.js
+++ src/components/tags/RegularTags.js
@@ -1,41 +1,27 @@
 import { Link } from 'react-router-dom'
+import { TAGS } from '../constants/tags'
 
 const getButtonClasses = (isActive) => (isActive && 'btn-primary') || 'btn-info'
+
+const TagLink = ({ tag, isActive }) => {
+  const tagUrl = '/tag/' + tag.url
+
+  return (
+    <Link to={tagUrl}>
+      <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses(isActive)}>
+        {tag.label}
+      </button>
+    </Link>
+  )
+}
 
 const RegularTags = (props) => {
   const tagCategory = props.category.replace('tag/', '')
   return (
     <>
-      <Link to='/tag/nft'>
-        <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses('nft' === tagCategory)}>
-          NFT
-        </button>
-      </Link>
-      <Link to='/tag/dao'>
-        <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses('dao' === tagCategory)}>
-          DAOs
-        </button>
-      </Link>
-      <Link to='/tag/blog'>
-        <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses('blog' === tagCategory)}>
-          Blogs
-        </button>
-      </Link>
-      <Link to='/tag/defi'>
-        <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses('defi' === tagCategory)}>
-          DeFi
-        </button>
-      </Link>
-      <Link to='/tag/nimi'>
-        <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses('nimi' === tagCategory)}>
-          Nimi
-        </button>
-      </Link>
-      <Link to='/tag/ens'>
-        <button type='button' className={'br-20 me-1 mb-3 px-4 btn ' + getButtonClasses('ens' === tagCategory)}>
-          ENS
-        </button>
-      </Link>
+      {TAGS.map((tag) => (
+        <TagLink key={tag.url} tag={tag} isActive={tag.url === tagCategory} />
+      ))}
     </>
   )
 }
