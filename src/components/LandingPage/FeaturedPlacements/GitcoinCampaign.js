7a88aa6a4262fe9622bfeea08741b1b95fa7d671
tomlightning <le.tomer@gmail.com>
2023-01-23 12:26:29 +0200

    Changed gitcoin campaign and show only gitcoin

--- src/components/LandingPage/FeaturedPlacements/GitcoinCampaign.js
+++ src/components/LandingPage/FeaturedPlacements/GitcoinCampaign.js
@@ -3,7 +3,10 @@ import LandingPageImg from '../../../images/gitcoin_bg.jpg'
 const GitcoinCampaign = () => {
   return (
     <div className='WotW'>
-      <a className='WotW-link' href='https://gitcoin.co/grants/4708/esteroids-ethereum-asteroids-a-project-for-eth-web'>
+      <a
+        className='WotW-link'
+        href='https://grant-explorer.gitcoin.co/#/round/1/0xd95a1969c41112cee9a2c931e849bcef36a16f4c/0x718ead66c1598336d76118607afea7d152b63b087e52f5b9c1f81398e4c06235-0xd95a1969c41112cee9a2c931e849bcef36a16f4c'
+      >
         <img className='mw-100' src={LandingPageImg} alt='TDW placeholder' />
         <div className='WotW-background'></div>
         <div className='WotW-header'>Support Esteroids</div>
