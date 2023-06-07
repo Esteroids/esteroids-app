a8a4393a6575451ecea936fca42c44398f2d1063
tomlightning <le.tomer@gmail.com>
2023-06-07 14:15:43 +0300

    svg as seperate images

--- src/components/constants/navbar.js
+++ src/components/constants/navbar.js
@@ -1,10 +1,19 @@
+import dWebsiteBotIcon from '../../images/svg/social/robot.svg'
+import discordIcon from '../../images/svg/social/discord.svg'
+import twitterIcon from '../../images/svg/social/twitter.svg'
+
 export const MAIN_NAV_BAR_ITEMS = [
   { label: 'Hot', link: '/hot', id: 'nav-link-2' },
   { label: 'New', link: '/new', id: 'nav-link-1' },
 ]
 
 export const MAIN_NAV_BAR_SOCIAL_ITEMS = [
-  { link: 'https://twitter.com/dwebsitesbot', class: 'twitter-icon', altLabel: 'Latest dWebsites bot', svgId: 'robot' },
-  { link: 'https://discord.gg/9c2EWzjFzY', class: 'discord-icon', altLabel: 'Our Discord', svgId: 'discord' },
-  { link: 'https://twitter.com/e_steroids', class: 'twitter-icon', altLabel: 'Our Twitter', svgId: 'twitter' },
+  {
+    link: 'https://twitter.com/dwebsitesbot',
+    class: 'twitter-icon',
+    altLabel: 'Latest dWebsites bot',
+    icon: dWebsiteBotIcon,
+  },
+  { link: 'https://discord.gg/9c2EWzjFzY', class: 'discord-icon', altLabel: 'Our Discord', icon: discordIcon },
+  { link: 'https://twitter.com/e_steroids', class: 'twitter-icon', altLabel: 'Our Twitter', icon: twitterIcon },
 ]
